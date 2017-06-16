8ba311aa72585b6a408ab14869f43860ea3e8d7b
crisauda <cristina.sauda@edu.unito.it>
2017-06-16 11:12:48 +0200

    analysis_studenti_scan_timingTP.C

--- Root-Macro/analysis_studenti_scan_timingTP.C
+++ Root-Macro/analysis_studenti_scan_timingTP.C
@@ -406,11 +406,150 @@ void TwoDim_Plots_If160_timing(Int_t i)
 	}
 }
 
+
+void TwoDim_Plots_If40_timing(Int_t i)
+{
+// ===========================================================================================================================
+    Float_t xmin, xmax, par0, par1, NEVT;
+	xmin = 30;
+	xmax = 300;
+	par0 = 50;
+	par1 = 2.;
+	NEVT = 50;
+	TString file,Dir, Dir0;
+	Int_t it=0;
+
+//	Dir0 = "data/CERN/board4/Annealed/Ifeed75-Vbl350/";
+
+    Dir0 = "C:/Users/CHIPIX65.CHIPIX65TO03/Documents/LabVIEW Data/CHIPIX_data/studenti/Board8/TPphase-scan-40MHz-Ifeed40/";	
+	
+    it=0;
+    Float_t tmax,tmin;
+    tmin=30.;
+    tmax=65;
+
+
+	while ( it<20 )  {
+		it++;
+		if (it==i ) {
+
+         if (it==1 ) {  par0=40; 
+		 
+   	   file =  "20170615210326_board8-CALsw-allch-i40-TP200_003."; 
+	   NEVT=50.;
+
+    }
+		if (it==2 ) {  par0=40; 
+		 
+   	   file =  "20170615211500_board8-CALsw-allch-i40-TP400_003."; 
+	   NEVT=50.;
+
+    }
+	if (it==3 ) {  par0=40; 
+		 
+   	   file =  "20170615212701_board8-CALsw-allch-i40-TP600_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==4	) {  par0=40; 
+		 
+   	   file =  "20170615213127_board8-CALsw-allch-i40-TP800_003."; 
+	   NEVT=50.;
+
+    }
+	if (it==5	) {  par0=40; 
+		 
+   	   file =  "20170615214253_board8-CALsw-allch-i40-TP1000_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==6 ) {  par0=40; 
+		 
+   	   file =  "20170615215454_board8-CALsw-allch-i40-TP12000_003."; 
+	   NEVT=50.;
+
+    }
+	if (it==7	) {  par0=40; 
+		 
+   	   file =  "20170615220656_board8-CALsw-allch-i40-TP1400_003."; 
+	   NEVT=50.;
+
+    }
+	
+	
+	if (it==8	) {  par0=40; 
+		 
+   	   file =  "20170615221527_board8-CALsw-allch-i40-TP1600_003."; 
+	   NEVT=50.;
+
+    }
+	
+    if (it==9	) {  par0=40; 
+		 
+   	   file =  "20170615222400_board8-CALsw-allch-i40-TP1800_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==10	) {  par0=40; 
+		 
+   	   file =  "20170615223232_board8-CALsw-allch-i40-TP2000_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==11	) {  par0=40; 
+		 
+   	   file =  "20170615224104_board8-CALsw-allch-i40-TP2200_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==12 ) {  par0=40; 
+		 
+   	   file =  "20170615224934_board8-CALsw-allch-i40-TP2400_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==13 ) {  par0=40; 
+		 
+   	   file =  "20170615225805_board8-CALsw-allch-i40-TP2600_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==14 ) {  par0=40; 
+		 
+   	   file =  "20170615230636_board8-CALsw-allch-i40-TP2800_003."; 
+	   NEVT=50.;
+
+    }
+	
+	if (it==15 ) {  par0=40; 
+		 
+   	   file =  "20170615231507_board8-CALsw-allch-i40-TP3000_003."; 
+	   NEVT=50.;
+
+    }
+	
+	
+	Dir = Dir0+"";	
+    Macro_scurve(Dir,file,xmin,xmax,par0,par1,NEVT);
+	analisiTHR2(Dir,file,320,tmin,tmax,0.,10.,23.); 
+    analisiMC(Dir,file,100,20,120,1.,8.); 
+	}
+	}
+}
+
 void Analysis(int i){
 
  
 //  TwoDim_Plots_40M_timing(i);
 //  TwoDim_Plots_20M_timing(i);
-  TwoDim_Plots_If160_timing(i);
+//  TwoDim_Plots_If160_timing(i);
+  TwoDim_Plots_If40_timing(i);
 
 }
